5966030213245b61178dff81ab6080cbfc14d037
Casey McNeil <caseymcneil@gmail.com>
2013-12-02 16:37:09 -0600

    Refactored code in the routes to render the views dy dynamically.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,40 +1,80 @@
 class MoviesController < ApplicationController
   
-  def show_gladiator
-    @movie = {
+
+
+  
+  # def show_gladiator
+  #   @movie = {
+  #     title: 'Gladiator',
+  #     subtitle: "Gladiator 'Are You Not Entertained' Scene",
+  #     video_id: 'FI1ylg4GKv8',
+  #     description: "Crowe shows everyone what's up."
+  #   }
+  # end
+
+  # def show_matilda
+  #   @movie = {
+  #     title: 'Matilda',
+  #     subtitle: "Big kid eating a brownie",
+  #     video_id: 'EVWOQwZENBg',
+  #     description: "Quite the random movie, but a classic for sure!"
+  #   }
+    
+  # end
+
+  # def show_groundhog
+  #   @movie = {
+  #     title: 'Groundhog Day',
+  #     subtitle: "Wait, what day is it?",
+  #     video_id: '9hq5jZrFTbE',
+  #     description: "It never ends!"
+  #   }
+  # end
+
+  # def show_gone
+  #   @movie = {
+  #     title: 'Gone with the Wind',
+  #     subtitle: "Rhett Butler doesn't give a damn.",
+  #     video_id: 'GQ5ICXMC4xY',
+  #     description: "Scartlet is a very vicious vixen."
+  #   }
+  # end
+
+  def show
+    movie_name = params[:movie_name]
+
+    movies = {
+      "gladiator" => {
       title: 'Gladiator',
       subtitle: "Gladiator 'Are You Not Entertained' Scene",
       video_id: 'FI1ylg4GKv8',
       description: "Crowe shows everyone what's up."
-    }
-  end
+      },
 
-  def show_matilda
-    @movie = {
+      "matilda" =>  {
       title: 'Matilda',
       subtitle: "Big kid eating a brownie",
       video_id: 'EVWOQwZENBg',
       description: "Quite the random movie, but a classic for sure!"
-    }
-    
-  end
+      },
 
-  def show_groundhog
-    @movie = {
+      "groundhog-day" => {
       title: 'Groundhog Day',
       subtitle: "Wait, what day is it?",
       video_id: '9hq5jZrFTbE',
       description: "It never ends!"
-    }
-  end
+      },
 
-  def show_gone
-    @movie = {
+      "gone-with-the-wind" => {
       title: 'Gone with the Wind',
       subtitle: "Rhett Butler doesn't give a damn.",
       video_id: 'GQ5ICXMC4xY',
       description: "Scartlet is a very vicious vixen."
+      }
+
     }
+
+    @movie = movies[movie_name]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 MetubeCohort3::Application.routes.draw do
-  #movies
-  get '/movies/gladiator' => 'movies#show_gladiator'
-  get '/movies/matilda' => 'movies#show_matilda'
-  get '/movies/groundhog-day' => 'movies#show_groundhog'
-  get '/movies/gone-with-the-wind' => 'movies#show_gone'
+  get "/movies/:movie_name" => 'movies#show', as: 'movie' 
 
-  #shows
-  get 'shows/24' => "shows#show_24"
-  get 'shows/seinfeld' => "shows#show_seinfeld"
-  get 'shows/breaking-bad' => "shows#show_breaking"
+  # root "movies#show_all", as: "home"
+  # #movies
+  # # get '/movies/gladiator' => 'movies#show_gladiator', as: "gladiator"
+  # # get '/movies/matilda' => 'movies#show_matilda', as: "matilda"
+  # get '/movies/groundhog-day' => 'movies#show_groundhog', as: "groundhog"
+  # get '/movies/gone-with-the-wind' => 'movies#show_gone', as: "gone"
+  # get '/movies' => 'movies#show_all', as: "all_movies"
+
+
+  # #shows
+  # get 'shows/24' => "shows#show_24", as: "twenty_four"
+  # get 'shows/seinfeld' => "shows#show_seinfeld", as: "seinfeld"
+  # get 'shows/breaking-bad' => "shows#show_breaking", as: "breaking"
 
 end
